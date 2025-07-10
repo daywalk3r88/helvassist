bdb33431bc23ea9cf9f0adb337a686c68f7c18d7
daywalk3r88 <bj.kieninger@hotmail.de>
2025-07-10 18:06:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-# Streamlit App
-
+# Streamlit App mit LlamaIndex + OpenAI
 import streamlit as st
+from llama_index import StorageContext, load_index_from_storage
 
+st.set_page_config(page_title="Helvetia GPT", layout="wide")
 st.title('Helvetia GPT')
 
+# Index laden
+storage_context = StorageContext.from_defaults(persist_dir="./storage")
+index = load_index_from_storage(storage_context)
+
+# Frageingabe
 query = st.text_input('Frage zur Versicherung:')
+
 if query:
-    st.write('Antwort kommt hier hin...')
+    # Anfrage an den Index
+    query_engine = index.as_query_engine()
+    response = query_engine.query(query)
+
+    # Ausgabe
+    st.write(response)
